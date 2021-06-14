ee8511d48894f37cbc0746d6dc52db43bd198cc4
rrahul <rahulbgc21@gmail.com>
2021-06-14 14:50:02 +0600

    menu changes stuff

---
+++ resources/lang/en/menu.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    //user management
+    'user_management' => 'User Management',
+    'users' => 'Users',
+    'user_type' => 'User Type',
+    'role' => 'Role',
+    'permissions' => 'Permissions',
+
+    //institute management
+    'institute_management' => 'Institute Management',
+    'institute' => 'Institute',
+    'branch' => 'Branch',
+    'training_center' => 'Training Center',
+    'programmes' => 'Programmes',
+
+    //course management
+    'course_management' => 'Course Management',
+    'courses' => 'Courses',
+    'application_form_types' => 'Application Form Types',
+    'course_config' => 'Course Config',
+
+    //Batch management
+    'batch_management' => 'Batch management',
+    'batch' => 'Batch',
+
+    //Application management
+    'application_management' => 'Application management',
+    'applied_student' => 'Applied Student',
+
+    //content management
+    'content_management' => 'Content Management',
+    'gallery' => 'Gallery',
+    'static_page' => 'Static Page',
+    'slider' => 'Slider',
+    'video' => 'Video',
+    'video_category' => 'Video Category',
+
+    //organization management
+    'organization_management'=> 'Organization Management',
+    'organizations'=> 'Organizations',
+    'organization_type'=> 'Organization Type',
+    'organization_units'=> 'Organization Units',
+    'organization_unit_types'=> 'Organization Unit Types',
+    'human_resource_templates' => 'HR Templates',
+    'ranks'=> 'Ranks',
+    'rank-type'=> 'Rank Type',
+
+    //skill management
+    'skill_management' => 'Skill Management',
+    'skills' => 'Skills',
+    'services' => 'Services',
+    'job_sectors' => 'Job Sectors',
+    'upazila_job_statistics' => 'Upazila Job Statistics',
+
+    //occupation management
+    'occupation_management' => 'Occupation Management',
+    'occupations' => 'Occupations',
+    'occupation_wise_statistics' => 'Occupation Wise Statistics',
+];
+
