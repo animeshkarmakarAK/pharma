3942f4cef3cf2f29efb74507e6d5353d7fb9defb
rrahul <rahulbgc21@gmail.com>
2021-06-14 14:50:02 +0600

    menu changes stuff

---
+++ resources/lang/bn/menu.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    //user management
+    'user_management' => 'ইউজার ম্যানেজমেন্ট',
+    'users' => 'ব্যবহারকারী',
+    'user_type' => 'ব্যবহারকারীর ধরন',
+    'role' => 'রোল',
+    'permissions' => 'অনুমতি',
+
+    //institute management
+    'institute_management' => 'ইনস্টিটিউট ম্যানেজমেন্ট',
+    'institute' => 'ইনস্টিটিউট',
+    'branch' => 'শাখা',
+    'training_center' => 'প্রশিক্ষণ কেন্দ্র',
+    'programmes' => 'প্রোগ্রাম',
+
+    //course management
+    'course_management' => 'কোর্স ম্যানেজমেন্ট',
+    'courses' => 'কোর্সসমূহ',
+    'application_form_types' => 'আবেদন ফর্মের ধরন',
+    'course_config' => 'কোর্স কনফিগার',
+
+    //batch management
+    'batch_management' => 'ব্যাচ ম্যানেজমেন্ট',
+    'batch' => 'ব্যাচ',
+
+    //application management
+    'application_management' => 'আবেদন ব্যবস্থাপনা',
+    'applied_student' => 'আবেদনকৃত ছাত্র/ছাত্রী',
+
+    //content management
+    'content_management' => 'কন্টেন্ট ম্যানেজমেন্ট',
+    'gallery' => 'গ্যালারী',
+    'static_page' => 'স্থির পৃষ্ঠা',
+    'slider' => 'স্লাইডার',
+    'video' => 'ভিডিও',
+    'video_category' => 'ভিডিও ধরন',
+
+    //organization management
+    'organization_management'=> 'সংস্থা পরিচালনা',
+    'organizations'=> 'সংস্থা',
+    'organization_type'=> 'সংস্থার ধরণ',
+    'organization_units'=> 'সংস্থা ইউনিট',
+    'organization_unit_types'=> 'সংস্থা ইউনিট প্রকার',
+    'human_resource_templates' => 'এইচআর টেম্পলেট',
+    'ranks'=> 'র‌্যাঙ্কস',
+    'rank-type'=> 'র‍্যংকের ধরন',
+
+    //skill management
+    'skill_management' => 'দক্ষতা পরিচালনা',
+    'skills' => 'দক্ষতা',
+    'services' => 'সেবা',
+    'job_sectors' => 'চাকরি সেক্টর',
+    'upazila_job_statistics' => 'উপজেলা চাকরির পরিসংখ্যান',
+
+    //occupation management
+    'occupation_management' => 'পেশা পরিচালনা',
+    'occupations' => 'পেশা',
+    'occupation_wise_statistics' => 'পেশাগত পরিসংখ্যান',
+];
+
