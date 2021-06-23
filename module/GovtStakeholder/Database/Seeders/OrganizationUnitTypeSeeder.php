5fb984183e0e78cdca2a57fa8f8408780a3baa17
Animesh Karmakar <animesh.pust@gmail.com>
2021-06-23 15:39:00 +0600

    add organization organization unit types. v.2

--- module/GovtStakeholder/Database/Seeders/OrganizationUnitTypeSeeder.php
+++ module/GovtStakeholder/Database/Seeders/OrganizationUnitTypeSeeder.php
@@ -5,6 +5,7 @@ namespace Module\GovtStakeholder\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Module\GovtStakeholder\App\Models\Organization;
 
 class OrganizationUnitTypeSeeder extends Seeder
 {
@@ -19,11 +20,13 @@ class OrganizationUnitTypeSeeder extends Seeder
         Schema::disableForeignKeyConstraints();
 
         DB::table('organization_unit_types')->truncate();
+        $organizations = Organization::pluck('id')->toArray();
 
         DB::table('organization_unit_types')->insert(array(
             0 =>
                 array(
                     'id' => 1,
+                    'organization_id' =>
                     'title_en' => 'Mobile Banking',
                     'title_bn' => 'মোবাইল ব্যাংকিং',
                     'row_status' => 1,
