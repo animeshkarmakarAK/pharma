f4f6659066e28d6423599fc5d2c74198dd67c9d3
Animesh Karmakar <animesh.pust@gmail.com>
2021-07-05 13:14:56 +0600

    youth profile now in bangla

--- module/CourseManagement/resources/views/frontend/youth/youth-profile.blade.php
+++ module/CourseManagement/resources/views/frontend/youth/youth-profile.blade.php
@@ -7,11 +7,11 @@
                 <div class="user-details card mb-3">
                     <div
                         class="card-header custom-bg-gradient-info">
-                        <div class="card-title float-left font-weight-bold text-primary">Youth Details</div>
+                        <div class="card-title float-left font-weight-bold text-primary">বিস্তারিত</div>
                         <div class="youth-access-key float-right d-inline-flex">
-                            <p class="label-text font-weight-bold">Access Key#: </p>
-                            <div>
-                                {{ $youth->access_key ?? 'N/A' }}
+                            <p class="label-text font-weight-bold">অ্যাক্সেস কি: </p>
+                            <div class="font-weight-bold">
+                                {{ "  ".$youth->access_key ?? 'N/A' }}
                             </div>
                         </div>
                     </div>
@@ -59,39 +59,39 @@
             <div class="col-md-8">
                 <div class="card bg-white">
                     <div class="card-header custom-bg-gradient-info text-primary">
-                        <h3 class="card-title font-weight-bold">{{ __('Youth Info') }}</h3>
+                        <h3 class="card-title font-weight-bold">তথ্য</h3>
 
                         <div class="card-tools">
                             <a href="#" class="btn btn-sm btn-outline-warning" onclick="Export()">
-                                <i class="fas fa-backward"></i> Download as PDF
+                                <i class="fas fa-backward"></i> ডাউনলোড পিডিএফ
                             </a>
                         </div>
                     </div>
 
                     <div class="card-body row">
                         <div class="col-md-6 custom-view-box">
-                            <p class="label-text">{{ __('Name(EN)') }}</p>
+                            <p class="label-text">নাম(ইংলিশ)</p>
                             <div class="input-box">
                                 {{ optional($youth)->name_en}}
                             </div>
                         </div>
 
                         <div class="col-md-6 custom-view-box">
-                            <p class="label-text">{{ __('Name(BN)') }}</p>
+                            <p class="label-text">নাম(বাংলা)</p>
                             <div class="input-box">
                                 {{ optional($youth)->name_bn}}
                             </div>
                         </div>
 
                         <div class="col-md-6 custom-view-box">
-                            <p class="label-text">{{ __('Email')}}</p>
+                            <p class="label-text">ইমেইল</p>
                             <div class="input-box">
                                 {{ optional($youth)->email ?? "N/A"}}
                             </div>
                         </div>
 
                         <div class="col-md-6 custom-view-box">
-                            <p class="label-text">{{ __('Mobile') }}</p>
+                            <p class="label-text">মোবাইল</p>
                             <div class="input-box">
                                 {{ $youth->mobile ?? "" }}
                             </div>
@@ -100,42 +100,42 @@
                 </div>
                 <div class="card bg-white">
                     <div class="card-header custom-bg-gradient-info text-primary">
-                        <h3 class="card-title font-weight-bold">{{ __('Youth Address') }}</h3>
+                        <h3 class="card-title font-weight-bold">ঠিকানা</h3>
                     </div>
                     <div class="card-body">
                         <div class="row mt-2">
                             <div class="col-md-6">
                                 <div class="card">
                                     <div class="card-header">
-                                        <div class="card-title">Present Address</div>
+                                        <div class="card-title">বর্তমান ঠিকানা</div>
                                     </div>
                                     <div class="card-body row">
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('Division') }}</p>
+                                            <p class="label-text">বিভাগ</p>
                                             <div class="input-box">
                                                 {{ optional($youth->presentAddressDivision)->title ?? "" }}
                                             </div>
                                         </div>
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('District') }}</p>
+                                            <p class="label-text">জেলা</p>
                                             <div class="input-box">
                                                 {{ optional($youth->presentAddressDistrict)->title ?? "" }}
                                             </div>
                                         </div>
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('Upazila') }}</p>
+                                            <p class="label-text">উপজেলা</p>
                                             <div class="input-box">
                                                 {{ optional($youth->presentAddressUpazila)->title ?? "" }}
                                             </div>
                                         </div>
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('Post Code') }}</p>
+                                            <p class="label-text">পোস্টঅফিস কোড</p>
                                             <div class="input-box">
                                                 {{ $youth->present_address_house_address['postal_code'] ?? "" }}
                                             </div>
                                         </div>
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('Village Name') }}</p>
+                                            <p class="label-text">গ্রাম</p>
                                             <div class="input-box">
                                                 {{ $youth->present_address_house_address['village_name'] ?? "" }}
                                             </div>
@@ -146,35 +146,35 @@
                             <div class="col-md-6">
                                 <div class="card">
                                     <div class="card-header">
-                                        <div class="card-title">Permanent Address</div>
+                                        <div class="card-title">স্থায়ী ঠিকানা</div>
                                     </div>
                                     <div class="card-body row">
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('Division') }}</p>
+                                            <p class="label-text">বিভাগ</p>
                                             <div class="input-box">
                                                 {{ $youth->permanentAddressDivision->title ?? "" }}
                                             </div>
                                         </div>
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('District') }}</p>
+                                            <p class="label-text">জেলা</p>
                                             <div class="input-box">
                                                 {{ optional($youth->permanentAddressDistrict)->title }}
                                             </div>
                                         </div>
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('Upazila') }}</p>
+                                            <p class="label-text">উপজেলা</p>
                                             <div class="input-box">
                                                 {{ optional($youth->permanentAddressUpazila)->title }}
                                             </div>
                                         </div>
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('Post Code') }}</p>
+                                            <p class="label-text">পোস্টঅফিস কোড</p>
                                             <div class="input-box">
                                                 {{ $youth->permanent_address_house_address['postal_code'] ?? "" }}
                                             </div>
                                         </div>
                                         <div class="col-md-6 custom-view-box">
-                                            <p class="label-text">{{ __('Village Name') }}</p>
+                                            <p class="label-text">গ্রাম</p>
                                             <div class="input-box">
                                                 {{ $youth->permanent_address_house_address['village_name'] ?? "" }}
                                             </div>
@@ -189,7 +189,7 @@
                 @if($haveYouthFamilyMembersInfo)
                     <div class="card">
                         <div class="card-header custom-bg-gradient-info">
-                            <div class="card-title text-primary font-weight-bold">Family Members Information</div>
+                            <div class="card-title text-primary font-weight-bold">পরিবারের সদস্যদের তথ্য</div>
                         </div>
                         <div class="card-body">
                             <div class="row">
@@ -197,30 +197,30 @@
                                     <div class="card">
                                         <div class="card-header">
                                             <div class="card-title">
-                                                Father
+                                                পিতা
                                             </div>
                                         </div>
                                         <div class="card-body row">
                                             <div class="col-md-6 custom-view-box">
-                                                <p class="label-text">{{ __('Name') }}</p>
+                                                <p class="label-text">নাম</p>
                                                 <div class="input-box">
                                                     {{ optional($father)->member_name_en }}
                                                 </div>
                                             </div>
                                             <div class="col-md-6 custom-view-box">
-                                                <p class="label-text">{{ __('Mobile') }}</p>
+                                                <p class="label-text">মোবাইল</p>
                                                 <div class="input-box">
                                                     {{ optional($father)->mobile}}
                                                 </div>
                                             </div>
                                             <div class="col-md-6 custom-view-box">
-                                                <p class="label-text">{{ __('NID') }}</p>
+                                                <p class="label-text">এন.আই.ডি</p>
                                                 <div class="input-box">
                                                     {{ optional($father)->nid }}
                                                 </div>
                                             </div>
                                             <div class="col-md-6 custom-view-box">
-                                                <p class="label-text">{{ __('Date of Birth') }}</p>
+                                                <p class="label-text">জন্মতারিখ</p>
                                                 <div class="input-box">
                                                     {{ !empty($father) ? \Illuminate\Support\Carbon::parse($father->date_of_birth)->format('d M Y') : ''}}
                                                 </div>
@@ -232,30 +232,30 @@
                                     <div class="card">
                                         <div class="card-header">
                                             <div class="card-title">
-                                                Mother
+                                                মাতা
                                             </div>
                                         </div>
                                         <div class="card-body row">
                                             <div class="col-md-6 custom-view-box">
-                                                <p class="label-text">{{ __('Name') }}</p>
+                                                <p class="label-text">নাম</p>
                                                 <div class="input-box">
                                                     {{ optional($mother)->member_name_en}}
                                                 </div>
                                             </div>
                                             <div class="col-md-6 custom-view-box">
-                                                <p class="label-text">{{ __('Mobile') }}</p>
+                                                <p class="label-text">মোবাইল</p>
                                                 <div class="input-box">
                                                     {{ optional($mother)->mobile}}
                                                 </div>
                                             </div>
                                             <div class="col-md-6 custom-view-box">
-                                                <p class="label-text">{{ __('NID') }}</p>
+                                                <p class="label-text">এন.আই.ডি</p>
                                                 <div class="input-box">
                                                     {{ optional($mother)->nid }}
                                                 </div>
                                             </div>
                                             <div class="col-md-6 custom-view-box">
-                                                <p class="label-text">{{ __('Date of Birth') }}</p>
+                                                <p class="label-text">জন্মতারিখ</p>
                                                 <div class="input-box">
                                                     {{  !empty($mother) ?  \Illuminate\Support\Carbon::parse($mother->date_of_birth)->format('d M Y') : '' }}
                                                 </div>
@@ -264,44 +264,44 @@
                                     </div>
                                 </div>
                                 @if(!empty($guardian) && $guardian == $father)
-                                    <p class="label-text font-weight-bold mr-1">Guardian: </p> Father
+                                    <p class="label-text font-weight-bold mr-1">অভিভাবক: </p> পিতা
                                 @elseif(!empty($guardian) && $guardian == $mother)
-                                    <p class="label-text font-weight-bold mr-1">Guardian: </p> Mother
+                                    <p class="label-text font-weight-bold mr-1">অভিভাবক: </p> মাতা
                                 @elseif(!empty($guardian))
                                     <div class="col-md-6">
                                         <div class="card">
                                             <div class="card-header">
                                                 <div class="card-title">
-                                                    Guardian
+                                                    অভিভাবক
                                                 </div>
                                             </div>
                                             <div class="card-body row">
                                                 <div class="col-md-6 custom-view-box">
-                                                    <p class="label-text">{{ __('Name') }}</p>
+                                                    <p class="label-text">নাম</p>
                                                     <div class="input-box">
                                                         {{ $guardian->member_name_en ?? "" }}
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6 custom-view-box">
-                                                    <p class="label-text">{{ __('Mobile') }}</p>
+                                                    <p class="label-text">মোবাইল</p>
                                                     <div class="input-box">
                                                         {{ $guardian->mobile ?? "" }}
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6 custom-view-box">
-                                                    <p class="label-text">{{ __('NID') }}</p>
+                                                    <p class="label-text">এন.আই.ডি</p>
                                                     <div class="input-box">
                                                         {{ $guardian->nid ?? "" }}
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6 custom-view-box">
-                                                    <p class="label-text">{{ __('Date of Birth') }}</p>
+                                                    <p class="label-text">জন্মতারিখ</p>
                                                     <div class="input-box">
                                                         {{ \Illuminate\Support\Carbon::parse($guardian->date_of_birth)->format('d M Y') ?? "" }}
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6 custom-view-box">
-                                                    <p class="label-text">{{ __('Relation with youth') }}</p>
+                                                    <p class="label-text">সম্পর্ক</p>
                                                     <div class="input-box">
                                                         {{ $guardian->relation_with_youth ?? "" }}
                                                     </div>
