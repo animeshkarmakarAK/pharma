f934b15d5c0b52e0048f00cbb8bb110a34f0468c
miladul <imiladul@gmail.com>
2021-07-08 15:28:42 +0600

    dashboard fix : Occupation Management

--- module/GovtStakeholder/resources/views/backend/occupations/read.blade.php
+++ module/GovtStakeholder/resources/views/backend/occupations/read.blade.php
@@ -6,6 +6,17 @@
         <div class="card">
             <div class="card-header text-primary custom-bg-gradient-info">
                 <h3 class="card-title">{{ __('Occupation') }}</h3>
+                <div class="card-tools">
+                    <div class="btn-group">
+                        <a href="{{route('govt_stakeholder::admin.occupations.edit', [$occupation->id])}}"
+                           class="btn btn-sm btn-outline-primary btn-rounded">
+                            <i class="fas fa-plus-circle"></i> {{ __('Edit Job Sector') }}
+                        </a>
+                        <a href="{{route('govt_stakeholder::admin.occupations.index')}}" class="btn btn-sm btn-outline-primary btn-rounded">
+                            <i class="fas fa-backward"></i> {{ __('Back to list') }}
+                        </a>
+                    </div>
+                </div>
             </div>
             <div class="row card-body">
                 <div class="col-md-6 custom-view-box">
