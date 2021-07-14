99b844e7cc625ce7c82b6bb3d28a27f1e906eaf8
Hasan <hasan.softbdltd@gmail.com>
2021-07-14 17:13:51 +0600

    quick fixing.

--- resources/views/acl/users/ajax/edit-add.blade.php
+++ resources/views/acl/users/ajax/edit-add.blade.php
@@ -75,7 +75,7 @@
                     </div>
                 </div>
 
-                @if($edit && $authUser->can('changeUserType', $user))
+                @if($authUser->isSuperUser() ||  $authUser->isSystemUser())
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="user_type_id">{{ __('User Type') }} <span
@@ -93,95 +93,96 @@
                             </select>
                         </div>
                     </div>
-                    @elseif(!$edit)
-                    <div class="col-sm-6">
+                    <div class="col-sm-6" style="display: none;">
                         <div class="form-group">
-                            <label for="user_type_id">{{ __('User Type') }} <span
+                            <label for="institute_id">{{ __('Institute') }} <span
                                     style="color: red"> * </span></label>
                             <select class="form-control select2-ajax-wizard"
-                                    name="user_type_id"
-                                    id="user_type_id"
-                                    data-model="{{base64_encode(App\Models\UserType::class)}}"
-                                    data-label-fields="{title}"
+                                    name="institute_id"
+                                    id="institute_id"
+                                    data-model="{{base64_encode(\Module\CourseManagement\App\Models\Institute::class)}}"
+                                    data-label-fields="{title_en}"
+                                    @if($edit && $user->institute)
+                                    data-preselected-option="{{json_encode(['text' =>  $user->institute->title_en, 'id' =>  $user->institute->id])}}"
+                                    @endif
                                     data-placeholder="নির্বাচন করুন"
                             >
                             </select>
                         </div>
                     </div>
+
+                    <div class="col-sm-6" style="display: none;">
+                        <div class="form-group">
+                            <label for="organization_id">{{ __('Organization') }} <span
+                                    style="color: red"> * </span></label>
+                            <select class="form-control select2-ajax-wizard"
+                                    name="organization_id"
+                                    id="organization_id"
+                                    data-model="{{base64_encode(\Module\GovtStakeholder\App\Models\Organization::class)}}"
+                                    data-label-fields="{title_en}"
+                                    @if($edit && $user->organization)
+                                    data-preselected-option="{{json_encode(['text' =>  $user->organization->title_en, 'id' =>  $user->organization->id])}}"
+                                    @endif
+                                    data-placeholder="নির্বাচন করুন"
+                            >
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6" style="display: none;">
+                        <div class="form-group">
+                            <label for="loc_district_id">{{ __('District') }} <span
+                                    style="color: red"> * </span></label>
+                            <select class="form-control select2-ajax-wizard"
+                                    name="loc_district_id"
+                                    id="loc_district_id"
+                                    data-model="{{base64_encode(\App\Models\LocDistrict::class)}}"
+                                    data-label-fields="{title_en}"
+                                    @if($edit && $user->locDistrict)
+                                    data-preselected-option="{{json_encode(['text' =>  $user->locDistrict->title_en, 'id' =>  $user->locDistrict->id])}}"
+                                    @endif
+                                    data-placeholder="নির্বাচন করুন"
+                            >
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6" style="display: none;">
+                        <div class="form-group">
+                            <label for="loc_division_id">{{ __('Division') }} <span
+                                    style="color: red"> * </span></label>
+                            <select class="form-control select2-ajax-wizard"
+                                    name="loc_division_id"
+                                    id="loc_division_id"
+                                    data-model="{{base64_encode(\App\Models\LocDivision::class)}}"
+                                    data-label-fields="{title_en}"
+                                    @if($edit && $user->locDivision)
+                                    data-preselected-option="{{json_encode(['text' =>  $user->locDivision->title_en, 'id' =>  $user->locDivision->id])}}"
+                                    @endif
+                                    data-placeholder="Select Division"
+                            >
+                                <option selected disabled>Select Division</option>
+                            </select>
+                        </div>
+                    </div>
+                @elseif($authUser->isInstituteUser())
+                    <input type="hidden" name="user_type_id" id="user_type_id"
+                           value="{{\App\Models\UserType::USER_TYPE_INSTITUTE_USER_CODE}}">
+                    <input type="hidden" name="institute_id" id="institute_id" value="{{$authUser->institute_id}}">
+                @elseif($authUser->isOrganizationUser())
+                    <input type="hidden" name="user_type_id" id="user_type_id"
+                           value="{{\App\Models\UserType::USER_TYPE_ORGANIZATION_USER_CODE}}">
+                    <input type="hidden" name="organization_id" id="organization_id"
+                           value="{{$authUser->organization_id}}">
+                @elseif($authUser->isDCUser())
+                    <input type="hidden" name="user_type_id" id="user_type_id"
+                           value="{{\App\Models\UserType::USER_TYPE_DC_USER_CODE}}">
+                    <input type="hidden" name="loc_district_id" id="loc_district_id" value="{{$authUser->loc_district_id}}">
+                @elseif($authUser->isDivcomUser())
+                    <input type="hidden" name="user_type_id" id="user_type_id"
+                           value="{{\App\Models\UserType::USER_TYPE_DIVCOM_USER_CODE}}">
+                    <input type="hidden" name="loc_division_id" id="loc_division_id" value="{{$authUser->loc_division_id}}">
                 @endif
-
-                <div class="col-sm-6" style="display: none;">
-                    <div class="form-group">
-                        <label for="institute_id">{{ __('Institute') }} <span
-                                style="color: red"> * </span></label>
-                        <select class="form-control select2-ajax-wizard"
-                                name="institute_id"
-                                id="institute_id"
-                                data-model="{{base64_encode(\Module\CourseManagement\App\Models\Institute::class)}}"
-                                data-label-fields="{title_en}"
-                                @if($edit && $user->institute)
-                                data-preselected-option="{{json_encode(['text' =>  $user->institute->title_en, 'id' =>  $user->institute->id])}}"
-                                @endif
-                                data-placeholder="নির্বাচন করুন"
-                        >
-                        </select>
-                    </div>
-                </div>
-
-                <div class="col-sm-6" style="display: none;">
-                    <div class="form-group">
-                        <label for="organization_id">{{ __('Organization') }} <span
-                                style="color: red"> * </span></label>
-                        <select class="form-control select2-ajax-wizard"
-                                name="organization_id"
-                                id="organization_id"
-                                data-model="{{base64_encode(\Module\GovtStakeholder\App\Models\Organization::class)}}"
-                                data-label-fields="{title_en}"
-                                @if($edit && $user->organization)
-                                data-preselected-option="{{json_encode(['text' =>  $user->organization->title_en, 'id' =>  $user->organization->id])}}"
-                                @endif
-                                data-placeholder="নির্বাচন করুন"
-                        >
-                        </select>
-                    </div>
-                </div>
-
-                <div class="col-sm-6" style="display: none;">
-                    <div class="form-group">
-                        <label for="loc_district_id">{{ __('District') }} <span
-                                style="color: red"> * </span></label>
-                        <select class="form-control select2-ajax-wizard"
-                                name="loc_district_id"
-                                id="loc_district_id"
-                                data-model="{{base64_encode(\App\Models\LocDistrict::class)}}"
-                                data-label-fields="{title_en}"
-                                @if($edit && $user->locDistrict)
-                                data-preselected-option="{{json_encode(['text' =>  $user->locDistrict->title_en, 'id' =>  $user->locDistrict->id])}}"
-                                @endif
-                                data-placeholder="নির্বাচন করুন"
-                        >
-                        </select>
-                    </div>
-                </div>
-
-                <div class="col-sm-6" style="display: none;">
-                    <div class="form-group">
-                        <label for="loc_division_id">{{ __('Division') }} <span
-                                style="color: red"> * </span></label>
-                        <select class="form-control select2-ajax-wizard"
-                                name="loc_division_id"
-                                id="loc_division_id"
-                                data-model="{{base64_encode(\App\Models\LocDivision::class)}}"
-                                data-label-fields="{title_en}"
-                                @if($edit && $user->locDivision)
-                                data-preselected-option="{{json_encode(['text' =>  $user->locDivision->title_en, 'id' =>  $user->locDivision->id])}}"
-                                @endif
-                                data-placeholder="Select Division"
-                        >
-                            <option selected disabled>Select Division</option>
-                        </select>
-                    </div>
-                </div>
 
                 @if($edit && $authUser->id == $user->id && $authUser->can('changePassword', $user))
                     <div class="col-sm-6">
