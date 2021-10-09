81bb0d907f2cbddbf8fbc32e891c73a0d2b2162f
Miladul Islam <imiladul@gmail.com>
2021-10-09 16:24:17 +0600

    Send notification to youth for payment success message by email done

---
+++ module/CourseManagement/App/Mail/YouthPaymentSuccessMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Module\CourseManagement\App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class YouthPaymentSuccessMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public string $msg;
+    public string $accessKey;
+    public string $youthName;
+
+    public function __construct($subject, $accessKey, $msg, $youthName)
+    {
+        $this->accessKey = $accessKey;
+        $this->msg = $msg;
+        $this->subject = $subject;
+        $this->youthName = $youthName;
+    }
+
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build(): YouthPaymentSuccessMail
+    {
+        return $this->from(env('MAIL_FROM_ADDRESS', 'noreply@skills.gov.bd'))
+            ->subject($this->subject)
+            ->view('course_management::frontend.email.youth-payment-success');
+    }
+}
