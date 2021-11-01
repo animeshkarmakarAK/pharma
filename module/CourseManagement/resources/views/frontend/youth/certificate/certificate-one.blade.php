137d64ea9dc2a0487739f17633acb8b56c0ac998
Miladul Islam <imiladul@gmail.com>
2021-11-01 11:15:20 +0600

    COURSE COORDINATOR & COURSE DIRECTOR signature fix

--- module/CourseManagement/resources/views/frontend/youth/certificate/certificate-one.blade.php
+++ module/CourseManagement/resources/views/frontend/youth/certificate/certificate-one.blade.php
@@ -161,12 +161,12 @@
                 </tr>
                 <tr>
                     <td colspan="2" style="text-align: left; padding-bottom: 50px">
-                        <img alt="signature1" src="{{ !empty($youthInfo)? $youthInfo['course_coordinator_signature']:'' }}" height="40px">
+                        <img alt="signature1" src="{{ !empty($youthInfo)? public_path($youthInfo['course_coordinator_signature']):'' }}" height="40px">
                         <p style="font-size: 16px; font-weight: bold;">COURSE COORDINATOR</p>
                     </td>
                     <td></td>
                     <td colspan="2" style="text-align: right; padding-bottom: 50px">
-                        <img alt="signature2" src="{{ !empty($youthInfo)? $youthInfo['course_director_signature']:'' }}" height="40px">
+                        <img alt="signature2" src="{{ !empty($youthInfo)? public_path($youthInfo['course_director_signature']):'' }}" height="40px">
                         <p style="font-size: 16px; font-weight: bold">COURSE DIRECTOR</p>
                     </td>
                 </tr>
